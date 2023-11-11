c3b8fec862abb07afc058276ac4ea25fa51084f7
CoderOfPrograms <62563579+CoderOfPrograms@users.noreply.github.com>
2023-11-11 18:48:47 -0500

    NavBar

--- townhall/app/shared/components/Navbar.tsx
+++ townhall/app/shared/components/Navbar.tsx
@@ -1,18 +1,30 @@
+// Import necessary dependencies
 import type { LinksFunction } from "@remix-run/node";
 import { Link } from "@remix-run/react";
-
 
 import stylesUrl from "~/styles/townhall.css";
 
 export const links: LinksFunction = () => [
-  { rel: "stylesheet", href: stylesUrl},
+  { rel: "stylesheet", href: stylesUrl },
 ];
 
 export default function Navbar() {
-    return (
-        <div className="header">
-            <Link className="name" to="/">TOWNHALL</Link>
-            <Link className="auth" to="/auth"> Log In</Link>
-        </div>
-    )
+  return (
+    <div className="header">
+      <Link className="name" to="/">
+        TOWNHALL
+      </Link>
+      <div className="center-links">
+        <Link className="nav-link" to="/problems">
+          Problems
+        </Link>
+        <Link className="nav-link" to="/add-proposal">
+          Add Proposal
+        </Link>
+      </div>
+      <Link className="auth" to="/auth">
+        Log In
+      </Link>
+    </div>
+  );
 }
